8a783d57914bf5d3204875d727f9b152bc759ced
HugoChollet <hugoc@bam.tech>
2023-01-20 14:50:02 +0100

    create checkbox component

---
+++ src/components/CheckBox/CheckBox.component.test.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { fireEvent, screen } from '@testing-library/react-native';
+
+import { wrapAndRender } from '@shared/helpers/jest/render';
+
+import { CheckBox } from './CheckBox.component';
+
+describe('CheckBox Component', () => {
+  it('should render the label', () => {
+    const mockOnPress = jest.fn();
+
+    wrapAndRender(
+      <CheckBox
+        label={'test'}
+        isChecked={false}
+        isDisabled={false}
+        onPress={mockOnPress}
+      />,
+    );
+
+    expect(screen.getByText('test')).toBeTruthy();
+  });
+
+  it(`should render the Checked icon when isChecked is true`, () => {
+    const mockOnPress = jest.fn();
+
+    wrapAndRender(
+      <CheckBox
+        label={'test'}
+        isChecked={true}
+        isDisabled={false}
+        onPress={mockOnPress}
+      />,
+    );
+
+    expect(screen.getByTestId('CheckBox-icon')).toBeTruthy();
+  });
+
+  it(`should render the Unchecked icon when isChecked is false`, () => {
+    const mockOnPress = jest.fn();
+
+    wrapAndRender(
+      <CheckBox
+        label={'test'}
+        isChecked={false}
+        isDisabled={false}
+        onPress={mockOnPress}
+      />,
+    );
+
+    expect(screen.getByTestId('UnCheckBox-icon')).toBeTruthy();
+  });
+
+  it('should call of onPress', () => {
+    const mockOnPress = jest.fn();
+
+    wrapAndRender(
+      <CheckBox
+        label={'test'}
+        isChecked={true}
+        isDisabled={false}
+        onPress={mockOnPress}
+      />,
+    );
+
+    fireEvent.press(screen.getByTestId('CheckBox-icon'));
+
+    expect(mockOnPress).toHaveBeenCalledTimes(1);
+  });
+
+  it(`should not call onPress when isDisabled is true`, () => {
+    const mockOnPress = jest.fn();
+
+    wrapAndRender(
+      <CheckBox
+        label={'test'}
+        isChecked={true}
+        isDisabled={true}
+        onPress={mockOnPress}
+      />,
+    );
+
+    fireEvent.press(screen.getByTestId('CheckBox-icon'));
+
+    expect(mockOnPress).toHaveBeenCalledTimes(0);
+  });
+});
---
+++ src/components/CheckBox/CheckBox.style.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import styled from '@emotion/native';
+import { useTheme } from '@emotion/react';
+
+import { Typography } from '../Typography/Typography.component';
+
+export const Container = styled.View({
+  flexDirection: 'row',
+  alignSelf: 'flex-start',
+});
+
+type LabelProps = { isDisabled: boolean; children: ReactNode };
+
+export const Label = ({ isDisabled, children }: LabelProps) => {
+  const theme = useTheme();
+
+  return (
+    <Typography.P1
+      color={isDisabled ? theme.colors.grey[300] : theme.colors.typography.text}
+    >
+      {children}
+    </Typography.P1>
+  );
+};
