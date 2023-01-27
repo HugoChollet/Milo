dea85fc66b630b11616ec9f4a11cf34ad9b2eb86
HugoChollet <hugoc@bam.tech>
2023-01-27 10:25:36 +0100

    add tests for buttons in Homepage

--- src/Home/HomeScreen.test.tsx
+++ src/Home/HomeScreen.test.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-import {render, screen} from '@testing-library/react-native';
-import {HomeScreen} from './HomeScreen';
+import { screen } from '@testing-library/react-native';
+import { HomeScreen } from './HomeScreen';
+import { wrapAndRender } from '../shared/jest/render';
 
-test('should verify two questions', () => {
-  render(<HomeScreen />);
+describe('Home Screen', () => {
+  test('should verify welcoming title', () => {
+    wrapAndRender(<HomeScreen />);
 
-  expect(screen.getByText('Welcome')).toBeTruthy();
+    expect(screen.getByText('Welcome')).toBeTruthy();
+  });
+
+  test('should verify two buttons', () => {
+    wrapAndRender(<HomeScreen />);
+
+    expect(screen.getByText('Login')).toBeTruthy();
+    expect(screen.getByText('Register')).toBeTruthy();
+  });
 });
