900164d9deb9415042f99177deac28ecc4ca2a66
HugoChollet <hugoc@bam.tech>
2023-02-06 12:00:40 +0100

    adjence buttons in homepage

--- src/Home/HomeScreen.tsx
+++ src/Home/HomeScreen.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import { Text, View } from 'react-native';
+import styled from '@emotion/native';
 
-export function HomeScreen() {
+import { Button } from '../components/Button/Button.component';
+import { Spacer } from '../components/Spacer/Spacer.component';
+
+const test = () => {
+  console.log('ok');
+};
+
+export const HomeScreen = () => {
   return (
-    <>
+    <Container>
       <Text>Welcome</Text>
-    </>
+      <View>
+        <Button.Primary label={'Login'} onPress={test} />
+        <Spacer.Vertical size={16} />
+        <Button.Primary label={'Register'} onPress={test} />
+      </View>
+    </Container>
   );
-}
+};
+
+const Container = styled.View(() => ({
+  flex: 1,
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'space-around',
+}));
