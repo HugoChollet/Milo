526dbb000b574931e8869035486cecbcb78be772
HugoChollet <hugoc@bam.tech>
2023-02-07 11:15:01 +0100

    setup react navigation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import { HomeScreen } from './src/Home/HomeScreen';
 import { theme } from './src/theme/theme';
+import { HomeScreen } from './src/Home/HomeScreen';
+import { LoginScreen } from './src/Login/LoginScreen';
 
 const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <HomeScreen />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
+          </Stack.Navigator>
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </NavigationContainer>
   );
 };
 
--- android/app/src/main/java/com/milo/MainActivity.java
+++ android/app/src/main/java/com/milo/MainActivity.java
@@ -3,6 +3,7 @@ package com.milo;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Milo";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
