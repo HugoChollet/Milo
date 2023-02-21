bcd8cc8b4d2c47ebf6a7cd435eead806db56ffd6
HugoChollet <hugoc@bam.tech>
2023-02-21 18:58:12 +0100

    basic dropdown for unit

--- src/Program/CreateProgram.tsx
+++ src/Program/CreateProgram.tsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useTheme } from '@emotion/react';
 import styled from '@emotion/native';
+import SelectDropdown from 'react-native-select-dropdown';
 
 import { CrossIcon } from '../icons/Cross.icon';
 import { IconButton } from '../components/IconButton/IconButton.component';
 import { ProgramData } from './ProgramDataType';
 import { Input } from '../components/Input/Input.component';
+import { theme } from '../theme/theme';
+import { StyleSheet } from 'react-native';
+
+const unit = ['m', 'km', 'km/h', 'g', 'kg', 's', 'min', 'unit'];
 
 export const CreateProgram = () => {
   const [program, setProgram] = useState<ProgramData>(null);
@@ -41,6 +46,14 @@ export const CreateProgram = () => {
           setProgram({ ...program, current: parseInt(text, 10) });
         }}
       />
+      <SelectDropdown
+        data={unit}
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        defaultValue="unit"
+        buttonStyle={style.dropdown}
+      />
       <Input
         label="Goal :"
         placeholder="10"
@@ -51,6 +64,14 @@ export const CreateProgram = () => {
     </Container>
   );
 };
+
+const style = StyleSheet.create({
+  dropdown: {
+    borderWidth: theme.spaces.xxs,
+    borderColor: theme.colors.grey[200],
+    margin: theme.spaces.s,
+  },
+});
 
 const Container = styled.View(({ theme }) => ({
   flex: 1,
