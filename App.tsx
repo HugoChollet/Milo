e3408fce149508d106db90ef1601921367b05b57
HugoChollet <tibolescabo@gmail.com>
2023-04-17 21:54:06 +0200

    simplify app to only have NavBar

--- App.tsx
+++ App.tsx
@@ -2,30 +2,16 @@ import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import { theme } from './src/theme/theme';
-import { HomeScreen } from './src/Home/HomeScreen';
-import { LoginScreen } from './src/Login/LoginScreen';
-
-import { RootStackParamList } from './src/types/RootStack';
-import { ProgramScreen } from './src/Program/ProgramScreen';
-import { EditProgramScreen } from './src/Program/EditProgramScreen';
+import { NavBar } from './src/Navigation/NavBar';
 
 const App = () => {
-  const Stack = createNativeStackNavigator<RootStackParamList>();
-
   return (
     <NavigationContainer>
       <SafeAreaProvider>
         <ThemeProvider theme={theme}>
-          <Stack.Navigator initialRouteName="Program">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Register" component={LoginScreen} />
-            <Stack.Screen name="Program" component={ProgramScreen} />
-            <Stack.Screen name="MakeProgram" component={EditProgramScreen} />
-          </Stack.Navigator>
+          <NavBar />
         </ThemeProvider>
       </SafeAreaProvider>
     </NavigationContainer>
