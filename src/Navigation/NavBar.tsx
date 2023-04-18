14c04370363481bb4e1d2eaea10a9017e489b8b4
HugoChollet <tibolescabo@gmail.com>
2023-04-18 14:57:56 +0200

    Add home and setting icon to navbar

--- src/Navigation/NavBar.tsx
+++ src/Navigation/NavBar.tsx
@@ -2,12 +2,28 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { SettingsScreen } from '../Settings/SettingScreen';
 import { StackNavigationProgram } from '../Program/StackNavigationProgram';
+import { HomeIcon } from '../icons/Home.icon';
+import { SettingIcon } from '../icons/Setting.icon';
+import { useTheme } from '@emotion/react';
 
 const Tab = createBottomTabNavigator();
 
 export const NavBar = () => {
+  const theme = useTheme();
+
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => {
+          if (route.name === 'Program') {
+            return <HomeIcon color={color} />;
+          } else if (route.name === 'Settings') {
+            return <SettingIcon color={color} />;
+          }
+        },
+        tabBarActiveTintColor: theme.colors.primary[500],
+        tabBarInactiveTintColor: theme.colors.grey[300],
+      })}>
       <Tab.Screen name="Program" component={StackNavigationProgram} />
       <Tab.Screen name="Settings" component={SettingsScreen} />
     </Tab.Navigator>
