eaff8c5a629d04414f5027987b0f7eb4b3120d0a
HugoChollet <tibolescabo@gmail.com>
2023-04-21 12:43:56 +0200

    create ProgramCardList component

---
+++ src/components/ProgramCardList/ProgramCardList.component.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FlatList } from 'react-native';
+import styled from '@emotion/native';
+
+import {
+  ProgramCard,
+  ProgramCardProps,
+} from '../ProgramCard/ProgramCard.component';
+
+const programCard = ({ item }: { item: ProgramCardProps }) => {
+  return (
+    <ProgramCard
+      title={item.title}
+      subtitle={item.subtitle}
+      image={item.image}
+      completion={item.completion}
+      onPlay={item.onPlay}
+      onView={item.onView}
+      onEdit={item.onEdit}
+      onDelete={item.onDelete}
+    />
+  );
+};
+
+type CardListProps = {
+  data: Array<ProgramCardProps>;
+};
+
+export const ProgramCardList = ({ data }: CardListProps) => {
+  return (
+    <Container>
+      <FlatList data={data} renderItem={programCard} />
+    </Container>
+  );
+};
+
+const Container = styled.View(({ theme }) => ({
+  flex: 1,
+  flexDirection: 'column',
+  paddingHorizontal: theme.spaces.m,
+}));
